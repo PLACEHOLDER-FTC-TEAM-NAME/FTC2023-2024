3161dd7d7d2144c146fa4356405aabb11f7ddf5a
SpectreDev <aidanta01@gmail.com>
2023-05-18 21:18:33 -0500

    Update MecanumDrive.java

--- TeamCode/src/main/java/net/nexusrobotics/swiftbot/drive/MecanumDrive.java
+++ TeamCode/src/main/java/net/nexusrobotics/swiftbot/drive/MecanumDrive.java
@@ -3,11 +3,14 @@ package net.nexusrobotics.swiftbot.drive;
 import net.nexusrobotics.swiftbot.odometry.Odometry;
 import net.nexusrobotics.swiftbot.robot.MecanumRobot;
 
+import java.util.HashMap;
+
 public class MecanumDrive {
     Odometry odometry;
     MecanumRobot robot;
     float targetX;
     float targetY;
+    float targetHeading;
     public MecanumDrive(Odometry odometry, MecanumRobot robot){
         this.odometry = odometry;
         this.robot = robot;
@@ -21,11 +24,30 @@ public class MecanumDrive {
 
     void loop(){
         new Thread(()->{
+            float toRotate = targetHeading - odometry.localization.currentHeading;
+            float toX = targetX - odometry.localization.currentX;
+            float toY = targetY - odometry.localization.currentY;
+
+            float forwardX = getOneFromForward(odometry.localization.currentHeading).get("x");
+            float forwardY = getOneFromForward(odometry.localization.currentHeading).get("y");
+            float rightX = getOneFromForward(odometry.localization.currentHeading + 90).get("x");
+            float rightY = getOneFromForward(odometry.localization.currentHeading + 90).get("y");
+
+
+
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
+    }
+
+    HashMap<String, Float> getOneFromForward(float heading){
+        heading = (float) Math.toRadians(heading);
+        HashMap<String, Float> map = new HashMap<>();
+        map.put("x", (float)Math.sin(heading));
+        map.put("y", (float)Math.cos(heading));
+        return map;
     }
 }
